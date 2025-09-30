d8d073c955ecdbd27ef0d1882fea4830957d983a
Amina-ds <omirtaykuralay@gmail.com>
2025-09-30 20:35:55 +0500

    Update 08_explain_analyze.sql

--- queries/08_explain_analyze.sql
+++ queries/08_explain_analyze.sql
@@ -1,18 +1,14 @@
 -- ЗАПРОС 8: Анализ планов и индексов
 
--- Наш "тяжелый" запрос для анализа
-SELECT * FROM emotion_events WHERE ts::date = '2025-09-10';
-
-
 -- План ДО индекса
 EXPLAIN ANALYZE
-SELECT * FROM emotion_events WHERE ts::date = '2025-09-10';
+SELECT * FROM emotion_events 
+WHERE ts >= '2025-09-10' AND ts < '2025-09-11';
 
 -- Создание индекса
 CREATE INDEX idx_emotion_events_ts ON emotion_events(ts);
 
 -- План ПОСЛЕ индекса
 EXPLAIN ANALYZE
-SELECT * FROM emotion_events WHERE ts::date = '2025-09-10';
-
-
+SELECT * FROM emotion_events 
+WHERE ts >= '2025-09-10' AND ts < '2025-09-11';
