02cbc0f7e5c7c92ac2418fc2959f50cfcbd909c4
Amina-ds <omirtaykuralay@gmail.com>
2025-09-30 20:05:38 +0500

    Update 09_constraints_check.sql

--- queries/09_constraints_check.sql
+++ queries/09_constraints_check.sql
@@ -1,1 +1,3 @@
-
+-- Демонстрация нарушения ссылочной целостности
+INSERT INTO emotion_events (class_id, student_id, ts, source, dominant_emotion_id, valence, arousal, confidence)
+VALUES (1, 9999, NOW(), 'video', 1, 0.5, 0.5, 0.9); -- ОШИБКА: student_id = 9999 не существует
